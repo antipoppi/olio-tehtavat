0b6435b5c296563a64ae599c984999403103da11
Tatu Alatalo <N4927@Labranet.jamk.fi>
2020-01-07 10:30:29 +0200

    T1-Koulunumero done

---
+++ Tehtavat/T1-Koulunumero/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1_Koulunumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //muuttujat
+            int point = 0;
+            int grade = 0;
+            string input;
+
+            //kysytään käyttäjältä pistemäärä
+            Console.WriteLine("Give points: ");
+            input = Console.ReadLine();
+            //muutetaan käyttäjän antama teksti (string) kokonaisluvuksi
+            point = int.Parse(input); //huom ei aina välttämättä onnistu, vaan voi aiheuttaa poikkeuksen
+            //pistemäärän perusteella arvosana
+            if (point < 2)
+            {
+                grade = 0;
+            }
+            else if (point <4)
+            {
+                grade = 1;
+            }
+            else if (point < 6)
+            {
+                grade = 2;
+            }
+            else if (point < 8)
+            {
+                grade = 3;
+            }
+            else if (point < 10)
+            {
+                grade = 4;
+            }
+            else
+            {
+                grade = 5;
+            }
+            //näytetään käyttäjälle
+            Console.WriteLine("School number is " + grade.ToString() + ".");
+            //loppu
+        }
+    }
+}
