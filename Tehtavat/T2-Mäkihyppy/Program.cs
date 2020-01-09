1054239440859472e5826f2af16a20c50e6152c1
N4927 <N4927@student.jamk.fi>
2020-01-09 15:51:02 +0200

    Removed unused systems from T1 and T2

--- Tehtavat/T2-Mäkihyppy/Program.cs
+++ Tehtavat/T2-Mäkihyppy/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace T2_Mäkihyppy
+namespace T2_Makihyppy
 {
     class Program
     {
