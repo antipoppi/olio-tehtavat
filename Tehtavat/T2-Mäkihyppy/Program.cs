9cb6d9f3388b583c6bf50d0dfc21e66b01fa70d1
N4927 <N4927@student.jamk.fi>
2020-01-09 14:21:52 +0200

    T2-Mäkihyppy done

---
+++ Tehtavat/T2-Mäkihyppy/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2_Mäkihyppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //muuttujat
+            string input;
+            int judgePoints = 0;
+            int pointHolder = 0;
+            int totalPoints = 0;
+            List<int> pointList = new List<int>();
+            //kysytään käyttäjältä tuomaripiste, lisätään se listaan ja toistetaan viidesti
+            for (int i=0; i<5; i++)
+            {
+                Console.Write("Give judge #" + (i+1) + " points: ");
+                input = Console.ReadLine();
+                int.TryParse(input,out judgePoints);
+                pointList.Add(judgePoints);
+            }
+            //järjestetään lista pisteiden mukaan pienimmästä suurimpaan
+            pointList.Sort();
+            //vähennetään pienin ja isoin piste listasta
+            pointList.RemoveAt(4);
+            pointList.RemoveAt(0);
+            //lasketaan taulukon sisältöjen summa
+            for (int i=0; i<pointList.Count; i++)
+            {
+                pointHolder = pointList[i];
+                totalPoints = pointHolder + totalPoints;
+            }
+            //tulostetaan konsoliin mäkihypyn kokonaispisteet
+            Console.WriteLine("Total points are " + totalPoints + " points.");
+            //loppu
+        }
+    }
+}
