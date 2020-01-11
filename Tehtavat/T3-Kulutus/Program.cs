ea7cc08d48fcd89fd152ab7ee8f000fa84715760
N4927 <N4927@student.jamk.fi>
2020-01-11 13:06:59 +0200

    T3-Kulutus done

---
+++ Tehtavat/T3-Kulutus/Program.cs
@@ -0,0 +1,39 @@
+﻿/* T3-Kulutus
+ * Tehtävänanto:
+ * 
+ * Auton kulutus on 7½ litraa 100 kilometrin matkalla ja bensan hinta on 1,55 Euroa.
+ * Tee metodi, jolla voi laskea tietyn ajomatkan bensakustannukset. 
+ * Pääohjelmassa kysytään käyttäjältä ajettu matka, sen jälkeen pääohjelma kutsuu metodia, lähettäen
+ * parametrina ajetun kilometrimäärän. Metodi palauttaa kuluvan bensan määrän sekä
+ * bensaan menevän rahan määrän ja pääohjelma näyttää ne käyttäjälle.
+ */
+
+using System;
+
+namespace T3_Kulutus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //muuttujat
+            string tripLenghtString;
+            double consumption = 0;
+            double cost = 0;
+            //kysytään käyttäjältä ajettu matka
+            Console.WriteLine("Give how many kilometers driven: ");
+            tripLenghtString = Console.ReadLine();
+            double.TryParse(tripLenghtString, out double tripLenght); //muutetaan matka-string doubleksi
+            //kutsutaan metodia
+            CountCost(tripLenght, out consumption, out cost);
+            //tulostetaan käyttäjälle kuluva bensa- ja rahamäärä
+            Console.WriteLine("Gasolin consumption for this trip is " + consumption + " liters and the cost of it is " + cost + " euros.");
+        }
+        public static void CountCost(double tripLength, out double consumption, out double cost)
+        {
+            consumption = (7.5 * tripLength) / 100;
+            cost = consumption * 1.55;
+            cost = Math.Round(cost, 2, MidpointRounding.AwayFromZero); //pyöristetään desimaalit lähimpään senttiin
+        }
+    }
+}
