d3a91527de387faa0dcbdcf19020643a9695b7c4
Tatu Alatalo <N4927@Labranet.jamk.fi>
2020-01-14 09:57:31 +0200

    added Demo00

---
+++ Demot/Demot/00Demo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _00Demo
+{
+    //a very simple Dog class
+    class Dog
+    {
+        //ominaisuudet (properties)
+        public string Name { get; set; }
+        //konstruktorit (constructors)
+        //HUOM: luokalla on olemassa ns. oletuskonstruktori
+        //mutta oletuskonstruktori häviää, kun määritellään omia konstruktoreita
+        public Dog()
+        { 
+        }
+        public Dog(string name)
+        {
+            Name = name;
+        }
+        //metodit (methods)
+        public string Growl()
+        {
+            //some interesting growling action happens here...
+            return "Murrr";
+        }
+        public string Bark(int times)
+        {
+            string bark = "";
+            for (int i = 0; i < times; i++)
+            {
+                bark += "vuh ";
+            }
+            return bark;
+        }
+    }
+
+    //DogDemo class
+    class DogDemo
+    {
+        //Main method
+        static void Main(string[] args)
+        {
+            //luodaan olio eli instanssi luokasta Dog. (create a new Dog object instance)
+            Dog dog = new Dog();
+            //set dog's name
+            dog.Name = "Hiro";
+            //show object's property and make him growl and bark calling method
+            Console.WriteLine("Koirani nimi on {0} ja se sanoo {1} {2}", dog.Name, dog.Growl(), dog.Bark(10));
+            //luodaan toinenkin koira
+            Dog puppy = new Dog("Miha"); //viitattiin Dog(string name) konstruktoriin.
+            Console.WriteLine("Naapurillani on koira nimeltä {0} ja se sanoo {1} {2}", puppy.Name, puppy.Growl(), puppy.Bark(3));
+        }
+    }
+}
