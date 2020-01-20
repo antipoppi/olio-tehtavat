b9cc87e4b0b89816fe2de8c815ab0fdc9629b268
N4927 <N4927@student.jamk.fi>
2020-01-20 21:15:13 +0200

    added T8-Televisio

---
+++ Tehtavat/T8-Televisio/Program.cs
@@ -0,0 +1,88 @@
+﻿/* T8-Televisio
+ * Tehtävänanto:
+ * 
+ * Tehtävänäsi on ohjelmoida television toiminta. Samoin kuin edellinen tehtävä:
+ * mitä ominaisuuksia ja toimintoja tekisit Televisio-luokkaan?
+ * Suunnittele ja piirrä Televisio-luokan ominaisuudet ja toiminnot UML-luokkakaaviona.
+ * Toteuta Televisio-luokan ohjelmointi sekä pääohjelma, jolla luot olion Televisio-luokasta.
+ * Säädä luomaasi Televsio-oliota erilaisilla arvoilla, jätä Console.WriteLine()-tulostuslauseet ohjelmaasi,
+ * jotta televisio-olion käyttäminen jää näkyville.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T8_Televisio
+{
+    class TV
+    {
+        // field variables
+        // properties
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int ProductionYear { get; set; }
+        public bool PowerStatus { get; set; }
+        public int SelectedChannel { get; set; }
+        public int CurrentVolume { get; set; }
+        public bool Teletext { get; set; }
+        // constructors
+        // methods
+        public bool TurnTVOn()
+        {
+            PowerStatus = true;
+            Console.WriteLine("TV is turned on");
+            return PowerStatus;
+        }
+        public bool TurnTVOff()
+        {
+            PowerStatus = false;
+            Console.WriteLine("TV is turned off");
+            return PowerStatus;
+        }
+        public int NextChannel()
+        {
+            SelectedChannel = SelectedChannel + 1;
+            Console.WriteLine("Next channel is {0}", SelectedChannel);
+            return SelectedChannel;
+        }
+        public int PreviousChannel()
+        {
+            SelectedChannel = SelectedChannel - 1;
+            Console.WriteLine("Previous channel is {0}", SelectedChannel);
+            return SelectedChannel;
+        }
+        public int SetChannel(int channelNmb)
+        {
+            SelectedChannel = channelNmb;
+            Console.WriteLine("Channel is now number {0}", SelectedChannel);
+            return SelectedChannel;
+        }
+        public int VolumeUp()
+        {
+            CurrentVolume += 5;
+            Console.WriteLine("Volume is now {0}", CurrentVolume);
+            return CurrentVolume;
+        }
+        public int VolumeDown()
+        {
+            CurrentVolume -= 5;
+            Console.WriteLine("Volume is now {0}", CurrentVolume);
+            return CurrentVolume;
+        }
+        public int VolumeMute()
+        {
+            CurrentVolume = 0;
+            Console.WriteLine("Volume is now muted");
+            return CurrentVolume;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
