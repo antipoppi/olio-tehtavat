c1053a23f0667d523d879920566610db7c0dadb4
Tatu Alatalo <N4927@Labranet.jamk.fi>
2020-01-21 10:32:50 +0200

    edited T8-Televisio

--- Tehtavat/T8-Televisio/Program.cs
+++ Tehtavat/T8-Televisio/Program.cs
@@ -20,88 +20,145 @@ namespace T8_Televisio
     class TV
     {
         // field variables
+        private readonly int maxChannel =  999;
+        private readonly int minChannel = 0;
+        private readonly int maxVolume = 100;
+        private readonly int minVolume = 0;
         // properties
-        public string Name { get; set; }
-        public string Model { get; set; }
-        public int ProductionYear { get; set; }
+        public string Name { get; }
+        public string Model { get; }
+        public int ProductionYear { get; }
         public bool PowerStatus { get; set; }
-        public int SelectedChannel { get; set; }
+        public int CurrentChannel { get; set; }
         public int CurrentVolume { get; set; }
         public bool TeletxtStatus { get; set; }
-        public int TeletextChnl { get; set; }
+        public int CurrentTeletextChannel { get; set; }
         // constructors
-        // methods
-        public bool TurnTVOn()
+        public TV()
         {
-            PowerStatus = true;
-            Console.WriteLine("TV is turned on");
-            return PowerStatus;
-        }
-        public bool TurnTVOff()
-        {
+            Name = "Samsung";
+            Model = "UE55M5005";
+            ProductionYear = DateTime.Now.Year;
             PowerStatus = false;
-            Console.WriteLine("TV is turned off");
+            CurrentChannel = 1;
+            CurrentVolume = 50;
+            TeletxtStatus = false;
+        }
+        // methods
+        public bool TogglePower()
+        {
+            if (PowerStatus == false)
+                PowerStatus = true;
+            else
+                PowerStatus = false;
             return PowerStatus;
         }
         public int NextChannel()
         {
-            SelectedChannel = SelectedChannel + 1;
-            Console.WriteLine("Next channel is {0}", SelectedChannel);
-            return SelectedChannel;
+            if (CurrentChannel + 1 <= maxChannel)
+            {
+                CurrentChannel = CurrentChannel + 1;
+                return CurrentChannel;
+            }
+            else
+            {
+                CurrentChannel = maxChannel;
+                return CurrentChannel;
+            }
         }
         public int PreviousChannel()
         {
-            SelectedChannel = SelectedChannel - 1;
-            Console.WriteLine("Previous channel is {0}", SelectedChannel);
-            return SelectedChannel;
+            if (CurrentChannel - 1 >= minChannel)
+            {
+                CurrentChannel = CurrentChannel - 1;
+                return CurrentChannel;
+            }
+            else
+            {
+                CurrentChannel = maxChannel;
+                return CurrentChannel;
+            }
         }
         public int SetChannel(int channelNmb)
         {
-            SelectedChannel = channelNmb;
-            Console.WriteLine("Channel is now number {0}", SelectedChannel);
-            return SelectedChannel;
+            CurrentChannel = channelNmb;
+            return CurrentChannel;
         }
         public int VolumeUp()
         {
-            CurrentVolume += 5;
-            Console.WriteLine("Volume is now {0}", CurrentVolume);
-            return CurrentVolume;
+            if (CurrentVolume + 5 <= maxVolume)
+            {
+                CurrentVolume = CurrentVolume + 5;
+                return CurrentVolume;
+            }
+            else
+            {
+                CurrentVolume = maxVolume;
+                return CurrentVolume;
+            }
         }
         public int VolumeDown()
         {
-            CurrentVolume -= 5;
-            Console.WriteLine("Volume is now {0}", CurrentVolume);
+            if (CurrentVolume - 5 >= minVolume)
+            {
+                CurrentVolume = CurrentVolume - 5;
+                return CurrentVolume;
+            }
+            else
+            {
+                CurrentVolume = minVolume;
+                return CurrentVolume;
+            }
+        }
+        public int MuteVolume()
+        {
+            CurrentVolume = minVolume;
             return CurrentVolume;
         }
-        public int VolumeMute()
+        public bool ToggleTeletext()
         {
-            CurrentVolume = 0;
-            Console.WriteLine("Volume is now muted");
-            return CurrentVolume;
-        }
-        public bool TeletextOn()
-        {
-            TeletxtStatus = true;
-            Console.WriteLine("Teletext is on");
-            return TeletxtStatus;
-        }
-        public bool TeletextOff()
-        {
-            TeletxtStatus = false;
-            Console.WriteLine("Teletext is off");
+            if (TeletxtStatus == false)
+                TeletxtStatus = true;
+            else
+                TeletxtStatus = false;
             return TeletxtStatus;
         }
         public int SetTeletextChannel(int teltxtNmb)
         {
-            TeletextChnl = teltxtNmb;
-            Console.WriteLine("Teletext channel is showing page {0}", TeletextChnl);
-            return TeletextChnl;
+            CurrentTeletextChannel = teltxtNmb;
+            return CurrentTeletextChannel;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            TV televisio = new TV();
+            televisio.TogglePower();
+            Console.WriteLine("Television virtastatus on {0}. (true on päällä, false pois päältä)", televisio.PowerStatus);
+            Console.WriteLine("Tämänhetkinen äänenvoimakkuustaso on {0} ja kanava on {1}.", televisio.CurrentVolume, televisio.CurrentChannel);
+            televisio.NextChannel();
+            Console.WriteLine("Tämän hetkinen kanava on {0}", televisio.CurrentChannel);
+            televisio.VolumeDown();
+            Console.WriteLine("Tämän hetkinen äänenvoimakkuus on {0}", televisio.CurrentVolume);
+            televisio.SetChannel(999);
+            Console.WriteLine("Tämän hetkinen kanava on {0}", televisio.CurrentChannel);
+            televisio.NextChannel(); // TÄÄ EI TOIMI
+            Console.WriteLine("Tämän hetkinen kanava on {0}", televisio.CurrentChannel);
+            televisio.MuteVolume();
+            Console.WriteLine("Tämän hetkinen äänenvoimakkuus on {0}", televisio.CurrentVolume);
+            televisio.ToggleTeletext();
+            Console.WriteLine("Tekstitv status on {0}. (true päällä, false pois päältä", televisio.TeletxtStatus);
+            televisio.SetTeletextChannel(666);
+            Console.WriteLine("Tekstitv sivu on {0}", televisio.CurrentTeletextChannel);
+            televisio.ToggleTeletext();
+            Console.WriteLine("Tekstitv status on {0}. (true päällä, false pois päältä", televisio.TeletxtStatus);
+            televisio.SetChannel(0);
+            Console.WriteLine("Tämän hetkinen kanava on {0}", televisio.CurrentChannel);
+            televisio.PreviousChannel();
+            Console.WriteLine("Tämän hetkinen kanava on {0}", televisio.CurrentChannel);
+            televisio.TogglePower();
+            Console.WriteLine("Television virtastatus on {0}. (true on päällä, false pois päältä)", televisio.PowerStatus);
         }
     }
 }
