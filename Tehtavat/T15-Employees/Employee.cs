7a04e333d1ef3ae43fd9f035d152f0a046feae62
Tatu Alatalo <N4927@student.jamk.fi>
2020-02-04 09:23:19 +0200

    T15-Employees add

---
+++ Tehtavat/T15-Employees/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T15_Employees
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public float Salary { get; set; }
+        // korvataan ToString-metodi omalla toteutuksella
+        public override string ToString()
+        {
+            return $"Työntekijä: {Name}, {Profession}, Palkka: {Salary} euroa"; 
+            // dollarimerkki=merkkijono jota "koostetaan" (class System.String)
+        }
+    }
+    class Boss : Employee
+    {
+        public string Car { get; set; }
+        public float Bonus { get; set; }
+    }
+}
---
+++ Tehtavat/T15-Employees/Program.cs
@@ -0,0 +1,39 @@
+﻿/* T15-Employees
+ * Tehtävänanto:
+ * 
+ * Ohjelmassa tulee pystyä käsittelemään työntekijöiden tietoja (Employee). 
+ * Työntekijöiden osalta seuraavia tietoa pitää pystyä käsittelemään: työntekijän nimi (Name), työntekijän ammatti (Profession) ja palkka (Salary). 
+ * Samassa ohjelmassa pitää pystyä käsittelemään myös johtajien tietoja (Boss), heillä on edellisten lisäksi myös auto (Car) ja palkkabonus (Bonus).
+ * Tutki tehtävän tavoitetta/kerrontaa ja toteuta tarvittavat UML-luokkakaaviot. Toteuta tämän jälkeen vaaditut luokat, luo ja käytä olioita pääohjelmasta. 
+ * Tulosta vaadittujen luokkien olioiden tietoja output-ikkunaan. Tietoja ei tarvitse kysyä sovelluksen käyttäjältä, vaan voit alustaa ne suoraan pääohjelmassa.
+ * 
+ */
+
+using System;
+using System.Collections.Generic; // for List<T>
+
+namespace T15_Employees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Firman Boss & Dumbs Ltd työntekijät ovat");
+            // luodaan pari työntekijää ja yksi pomo
+            Employee employee = new Employee();
+            employee.Name = "Kirsi Kernel";
+            employee.Profession = "Programmer";
+            employee.Salary = 3000F;
+            Employee employee2 = new Employee() { Name = "Matti Mainio", Profession = "Developer", Salary = 3000F };
+            // työntekijät listaan
+            List<Employee> staff = new List<Employee>();
+            staff.Add(employee); // Kirsin tiedot sisältävä olio lisätään listaan employees
+            staff.Add(employee2); // Matin tiedot -::-
+            // näytetään kaikki listan työntekijät
+            foreach (Employee item in staff)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
