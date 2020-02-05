c069784a73010d10b91c34c3df8251303b6f3e0d
N4927 <N4927@student.jamk.fi>
2020-02-05 14:13:52 +0200

    T11-CD done

--- Tehtavat/T11-CD/Program.cs
+++ Tehtavat/T11-CD/Program.cs
@@ -39,7 +39,7 @@ namespace T11_CD
         public override string ToString()
         {
             return $"CD: \n- Artist: " + Artist + " \n- Name: " + Name + " \n- Genre: " + Genre + " \n- Price: " 
-                + Price + "€\nSongs:\n" + string.Join("\n", Songs) + "\n- CopyProtection ID: " + CopyProtectionId;
+                + Price + "€\nSongs:\n" + string.Join("\n", Songs) + "\n- CopyProtection ID: " + CopyProtectionId + "\n";
         }
     }
     class Program
@@ -47,6 +47,7 @@ namespace T11_CD
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8; // this allows showing € in the console
+            // creating cd1
             CD cd1 = new CD() { Artist = "Metallica", Name = "Ride the Lightning", Genre = "Metal", Price = 19.9F };
             List<string> songList = new List<string>();
             songList.Add("--- Name: Fight Fire with Fire (4:45)");
@@ -59,6 +60,19 @@ namespace T11_CD
             songList.Add("--- Name: The Call of Ktulu (8:53)");
             cd1.Songs = songList;
             Console.WriteLine(cd1.ToString());
+            // creating cd2
+            CD cd2 = new CD() { Artist = "Natalia Lafourcade", Name = "Hasta la Raíz", Genre = "Latin pop", Price = 17.9F };
+            List<string> songList2 = new List<string>();
+            songList2.Add("--- Name: Hasta la Raíz (3:41)");
+            songList2.Add("--- Name: Mi Lugar Favorito (4:57)");
+            songList2.Add("--- Name: Antes de Huir (3:52)");
+            songList2.Add("--- Name: Ya No Te Puedo Querer (4:47)");
+            songList2.Add("--- Name: Para Qué Sufrir (3:46)");
+            songList2.Add("--- Name: Nunca es suficiente (3:57)");
+            songList2.Add("--- Name: Palomas Blancas (4:47)");
+            songList2.Add("--- Name: Te Quiero Ver (3:27)");
+            cd2.Songs = songList2;
+            Console.WriteLine(cd2.ToString());
         }
     }
 }
