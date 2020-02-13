c8bc7d1362144b9e2888234d981d6c0dc92f59c2
N4927 <N4927@student.jamk.fi>
2020-02-13 20:14:28 +0200

    T12-Tank done

--- Tehtavat/T12-Tank/Program.cs
+++ Tehtavat/T12-Tank/Program.cs
@@ -1,9 +1,9 @@
 ﻿/* T12-Tank
  * Tehtävänanto:
- * oteuta luokka Tank, jolla on seuraavat ominaisuudet: 
+ * toteuta luokka Tank, jolla on seuraavat ominaisuudet: 
  * Name (string), Type (string), CrewNumber(int), Speed(float), SpeedMax(float)
  * sekä metodit:
- * AccerelateTo(float), SlowTo(float)
+ * AccelerateTo(float), SlowTo(float)
  *
  * Ominaisuudet Name ja Type ovat luettavissa sekä muutettavissa ilman rajoituksia.
  * 
@@ -25,14 +25,14 @@ namespace T12_Tank
     {
         // field variables
         private readonly int defaultCrewNumber = 4;
+        private int crew;
         private readonly float maxSpeed = 100;
         private readonly float minSpeed = 0;
-        private readonly float accerelate = 25;
+        private float currentSpeed;
 
         // properties
         public string Name { get; set; }
         public string Type { get; set; }
-        private int crew;
         public int CrewNumber // crewnumber can only be between 2 and 6, default is 4
         {
             get { return crew; }
@@ -49,48 +49,69 @@ namespace T12_Tank
                 }
             }
         }
-        private float currentSpeed;
-        private float speed;
         public float Speed
         {
-            get { return minSpeed; }
-            set { speed = currentSpeed; }
+            get { return currentSpeed; }
         }
         public float SpeedMax
         {
             get { return maxSpeed; }
         }
-        // methods
-        public float AccerelateTo(float accerelate)
+
+        // constructors
+        public Tank() // in default, there should always be 4 crew members in the tank
         {
-            if (Speed >= 0 && Speed <= SpeedMax)
+            CrewNumber = defaultCrewNumber;
+        }
+
+        // methods
+        public float AccelerateTo(float accelerate)
+        {
+            if ((accelerate > minSpeed) && (accelerate <= SpeedMax)) // accelerate should be between min and max speed
             {
-                currentSpeed += accerelate;
-                Speed = currentSpeed;
+                currentSpeed = accelerate;
                 return Speed;
             }
-            else
+            else // else no change in speed
                 return Speed;
         }
-        public void SlowTo()
+        public float SlowTo(float slowdown)
         {
-
+            if (slowdown < Speed && slowdown > minSpeed) // slowdown should be between current and minimum speed
+            {
+                currentSpeed = slowdown;
+                return Speed;
+            }
+            else // if not, no change in speed
+                return Speed;
         }
-
-
+        public override string ToString()
+        {
+            return $"This {Name} is a {Type}-type of tank and it has {CrewNumber} crew members aboard. Its current speed is {Speed} of {SpeedMax}." ;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Tank tankki = new Tank() { CrewNumber = 5 };
-            tankki.AccerelateTo(25);
-            tankki.AccerelateTo(25);
-            tankki.AccerelateTo(25);
-            tankki.AccerelateTo(25);
-            tankki.AccerelateTo(25);
-            tankki.AccerelateTo(25);
-            Console.WriteLine(tankki.Speed);
+            Tank tankki = new Tank() { Type = "Heavy", Name = "Tankki", CrewNumber = 5 };
+            // printing info of the tank
+            Console.WriteLine(tankki.ToString());
+            // accelerating speed to 75
+            tankki.AccelerateTo(75);
+            Console.WriteLine("Tanks current speed is: " + tankki.Speed);
+            // trying to accelerating speed to 125
+            tankki.AccelerateTo(125);
+            Console.WriteLine("Tanks current speed is: " + tankki.Speed); // should still be 75
+            // slow it down to 33.
+            tankki.SlowTo(33);
+            Console.WriteLine("Tanks current speed is: " + tankki.Speed); // should be 33
+            // trying to stop the tank by giving a negative value (reverse is not valid in this tank)
+            tankki.SlowTo(-5);
+            Console.WriteLine(tankki.ToString());
+            // changing number of crewmembers to 7, should be 4 after because max is 6
+            tankki.CrewNumber = 7;
+            Console.WriteLine(tankki.ToString());
         }
     }
 }
