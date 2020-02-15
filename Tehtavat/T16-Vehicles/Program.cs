1d969aa58b94e2d4ca472c7b8f2b085c738b6354
N4927 <N4927@student.jamk.fi>
2020-02-15 19:39:47 +0200

    added T16-Vehicles

---
+++ Tehtavat/T16-Vehicles/Program.cs
@@ -0,0 +1,27 @@
+﻿/* T16-Vehicles
+ * Tehtävänanto:
+ * Toteutettavassa sovelluksessa tulisi pysyä käsittelemään erilaisia kulkuneuvoja. 
+ * Kaikilla kulkuneuvoilla on löydettävissä yhteisinä ominaisuuksina: nimi, malli, vuosimalli ja väri. 
+ * Erikoistapauksina pitää pystyä käsittelemään polkupyöriä ja veneitä. 
+ * Polkupyörän osalta pitää pystyä erottelemaan se, että onko kyseessä vaihdepyörä vai ei sekä mahdollisen vaihteiston mallinimi. 
+ * Veneiden osalta tietoina pitää ainakin olla veneen tyyppi (soutuvene, moottorivene, kajakki, ...) ja kuinka monta istuinpaikkaa veneestä löytyy.
+ * 
+ * Tutki tehtävän tavoitetta/kerrontaa ja toteuta tarvittavat UML-luokkakaaviot. 
+ * Toteuta tämän jälkeen vaaditut luokat, luo ja käytä olioita pääohjelmasta. 
+ * Tulosta vaadittujen luokkien olioiden tietoja output-ikkunaan. 
+ * Tietoja ei tarvitse kysyä sovelluksen käyttäjältä, vaan voit alustaa ne suoraan pääohjelmassa.
+ */
+
+using System;
+
+namespace T16_Vehicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // lähde liikkeelle UML-luokkakaaviosta
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
