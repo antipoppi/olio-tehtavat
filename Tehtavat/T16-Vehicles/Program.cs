ae56097c8cb0870260742e391cca2e21a83fc842
Tatu Alatalo <N4927@student.jamk.fi>
2020-02-18 10:28:46 +0200

    T16-Vehicles done

--- Tehtavat/T16-Vehicles/Program.cs
+++ Tehtavat/T16-Vehicles/Program.cs
@@ -13,15 +13,62 @@
  */
 
 using System;
+using System.Collections.Generic;
 
 namespace T16_Vehicles
 {
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int ProductionYear { get; set; }
+        public string Colour { get; set; }
+        public override string ToString()
+        {
+            return $"\nKulkuneuvon tiedot: \nNimi: {Name} \nMalli: {Model} \nValmistusvuosi: {ProductionYear} \nVäri: {Colour}";
+        }
+    }
+    class Bicycle : Vehicle
+    {
+        public bool Gear { get; set; }
+        public string GearName { get; set; }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nVaihteet: {Gear} \nVaihteiden nimi: {GearName}";
+        }
+    }
+    class Boat : Vehicle
+    {
+        public string Type { get; set; }
+        public int SeatAmount { get; set; }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nTyyppi: {Type} \nIstuinpaikkojen määrä: {SeatAmount}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            // lähde liikkeelle UML-luokkakaaviosta
-            Console.WriteLine("Hello World!");
+            // luodaan kulkuneuvo-olioita
+            Vehicle auto = new Vehicle() { Name = "Porche", Model = "Coupe", Colour = "Yellow", ProductionYear = 2005 };
+            // luodaan vaihteeton polkupyörä
+            Bicycle pyörä1 = new Bicycle() { Name = "Helkama", Model = "Tunturi", Colour = "Blue", ProductionYear = 1985, Gear = false };
+            // luodaan vaihteellinen polkupyörä
+            Bicycle pyörä2 = new Bicycle() { Name = "Baana", Model = "Dyyni", Colour = "White", ProductionYear = 2003, Gear = true, GearName="Shimano - 3 gears" };
+            // luodaan vene jossa 3 istumapaikkaa
+            Boat vene = new Boat() { Name = "Asseri", Model = "420", Colour = "Green", ProductionYear = 2010, Type = "rowing boat", SeatAmount = 3 };
+            // luodaan lista johon lisätään oliot
+            List<Vehicle> vehiclesList = new List<Vehicle>();
+            vehiclesList.Add(auto);
+            vehiclesList.Add(pyörä1);
+            vehiclesList.Add(pyörä2);
+            vehiclesList.Add(vene);
+            // tulostetaan listan sisältö
+            foreach (Vehicle item in vehiclesList)
+            {
+                Console.WriteLine(item.ToString());
+            }
         }
     }
 }
