3777e97c4ed60e94ac8c068c351684254bfa09d1
N4927 <N4927@student.jamk.fi>
2020-03-06 20:18:58 +0200

    T17-Radio done

--- Tehtavat/T17-Radio/Program.cs
+++ Tehtavat/T17-Radio/Program.cs
@@ -16,35 +16,95 @@ namespace T17_Radio
     {
         // properties
         public bool PowerStatus { get; set; }
+        public int Volume { get; set; }
+        public double ChannelFreq { get; set; }
+    }
+    class PortableRadio : Radio
+    {
+        // properties
+
         // constructors
-        public Radio()
+        public PortableRadio()
         {
             PowerStatus = false;
         }
         // methods
-        public string TogglePower()
+        public void TogglePower()
         {
             if (PowerStatus == false)
             {
                 PowerStatus = true;
-                return $"Radio on nyt päällä";
             }
             else
             {
                 PowerStatus = false;
-                return $"Radio on pois päältä";
+                Volume = 0;
+                ChannelFreq = 0;
             }
+        }
+        public void SetVolume(int vol)
+        {
+            if (vol >= 0 && vol <= 9 && PowerStatus == true)
+                Volume = vol;
+
+            else if (vol < 0 && PowerStatus == true)
+                Volume = 0;
+
+            else if (vol > 9 && PowerStatus == true)
+                Volume = 9;
+        }
+        public void SetChannel(double chan)
+        {
+            if (chan >= 2000 && chan <= 26000 && PowerStatus == true)
+                ChannelFreq = chan;
+
+            else if (chan < 2000 && PowerStatus == true)
+                ChannelFreq = 2000;
+
+            else if (chan > 26000 && PowerStatus == true)
+                ChannelFreq = 26000;
+        }
+        public string PrintPower()
+        {
+            if (PowerStatus == false)
+                return $"Radio on pois päältä";
+            else
+                return $"Radio on päällä";
         }
         public override string ToString()
         {
-            return $"{TogglePower()}";
+            return $"{PrintPower()}. Äänenvoimakkuus on {Volume}. Kanavataajuus on {ChannelFreq}.";
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Radio kannettavaRadio = new Radio();
+            // luodaan radio
+            PortableRadio kannettavaRadio = new PortableRadio();
+            // tulostetaan tiedot konsoliin
+            Console.WriteLine(kannettavaRadio.ToString());
+            // yritetään muuttaa äänenvoimakkuus arvoon 5 ja säätää kanava taajuudelle 22000
+            kannettavaRadio.SetVolume(5);
+            kannettavaRadio.SetChannel(22000);
+            // tulostuksessa pitäisi lukea äänenvoimakkuuden sekä kanavataajuuden olevan arvossa nolla
+            Console.WriteLine(kannettavaRadio.ToString());
+            // laitetaan radio päälle
+            kannettavaRadio.TogglePower();
+            // tulostetaan tiedot konsoliin
+            Console.WriteLine(kannettavaRadio.ToString());
+            // muutetaan äänenvoimakkuus arvoon 5 ja säädetään kanava taajuudelle 22000
+            kannettavaRadio.SetVolume(5);
+            kannettavaRadio.SetChannel(22000);
+            // tulostuksessa pitäisi lukea äänenvoimakkuuden olevan arvossa 5 ja kanavataajuus olevan 22000
+            Console.WriteLine(kannettavaRadio.ToString());
+            // muutetaan äänenvoimakkuus arvoon -5 ja säädetään kanava taajuudelle 30000
+            kannettavaRadio.SetVolume(-5);
+            kannettavaRadio.SetChannel(30000);
+            // tulostuksessa pitäisi lukea äänenvoimakkuuden olevan arvossa 0 ja kanavataajuus olevan 26000
+            Console.WriteLine(kannettavaRadio.ToString());
+            // suljetaan radio ja tulostetaan tiedot konsoliin (äänenvoimakkuus 0 ja kanavataajuus 0)
+            kannettavaRadio.TogglePower();
             Console.WriteLine(kannettavaRadio.ToString());
         }
     }
