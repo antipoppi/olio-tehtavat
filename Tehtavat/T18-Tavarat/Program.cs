01e989545bb953d06d61d200c1f89416999a2f44
N4927 <N4927@student.jamk.fi>
2020-03-06 20:33:49 +0200

    added T18-Tavarat and T19-Perintä

---
+++ Tehtavat/T18-Tavarat/Program.cs
@@ -0,0 +1,23 @@
+﻿/* T18-Tavarat
+ * Tehtävänanto:
+ * ICT-opiskelijan kirjahyllystä löytyy vaikka mitä erilaisia tavaroita: kirjoja, lehtiä,
+ * cd-levyjä, dvd-levyjä, bluray-levyjä, puhelimia, tabletteja, läppäreitä, ... jne.
+ * 
+ * Pohdi UML-kaaviota käyttäen millaisia luokkarakenteita (ainakin luokkien ja ominaisuuksien osalta)
+ * esiintyy ja käytä apua perintää, jos tavaroille löytyy yhteisiä ominaisuuksia.
+ * Toteuta tämän jälkeen muutamia luokkia, joissa perintää esiintyy. Tee myös pääohjelma,
+ * jossa käytät tekemiäsi luokkia ja luot olioita.
+ */
+
+using System;
+
+namespace T18_Tavarat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tehtavat/T19-Perintä/Program.cs
@@ -0,0 +1,17 @@
+﻿/* T19-Perintä
+ * Tehtävänanto:
+ * Pohdi jokin reaalimaailman asia, jonka kautta voit toteuttaa pienimuotoisen C#-ohjelman/toteutuksen, joka käyttää perintää.
+ */
+
+using System;
+
+namespace T19_Perintä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
