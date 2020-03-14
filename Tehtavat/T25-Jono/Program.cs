79a80602f56274dcae91e19cff1a65a113f0867b
N4927 <N4927@student.jamk.fi>
2020-03-14 15:59:47 +0200

    added T25-Jono

---
+++ Tehtavat/T25-Jono/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T25_Jono
+{
+    class Asiakkaat
+    {
+        public string Name { get; set; }
+    }
+    class Kassajono
+    {
+        // properties
+        public Queue<Asiakkaat> Jono { get; private set; }
+        public int Pituus
+        {
+            get
+            {
+                return Jono.Count;
+            }
+        }
+        // constructors
+        public Kassajono()
+        {
+            Jono = new Queue<Asiakkaat>();
+        }
+        // methods
+        public void MeneJonoon(Asiakkaat asiakas)
+        {
+            Jono.Enqueue(asiakas);
+        }
+        public void PoistuJonosta(Asiakkaat asiakas)
+        {
+            Jono.Dequeue();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
