4efe8ac2a3509e7efb2db49cc3f9dbf4a0575b31
N4927 <N4927@student.jamk.fi>
2020-03-14 17:12:44 +0200

    T25-Jono done

--- Tehtavat/T25-Jono/Program.cs
+++ Tehtavat/T25-Jono/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/* T25-Jono
+ * Tehtävänanto:
+ * 
+ * Toteuta ratkaisu, joka simuloi kaupan kassan asiakasvirtaa. Toteuta luokka Kassajono joka käyttää jonoa eli Queue-tietorakennetta.
+ * Kassajono -luokalla voisi olla esimerkiksi metodit: 
+ * MeneJonoon(parametrit)
+ * PoistuJonosta(parametrit)
+ * ja ominaisuus: Pituus
+ * 
+ * Voit myös suunnitella ja toteuttaa oman rajapinnan luokalle. 
+ * Huom: tässä yhteydessä rajapinta tarkoittaa luokasta ulospäin näkyviä julkisia ominaisuuksia ja metodeja.
+ */
+using System;
 using System.Collections.Generic;
 
 namespace T25_Jono
@@ -6,6 +18,10 @@ namespace T25_Jono
     class Asiakkaat
     {
         public string Name { get; set; }
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
     }
     class Kassajono
     {
@@ -28,16 +44,44 @@ namespace T25_Jono
         {
             Jono.Enqueue(asiakas);
         }
-        public void PoistuJonosta(Asiakkaat asiakas)
+        public string MaksaOstokset(Asiakkaat asiakas)
         {
-            Jono.Dequeue();
+            return $"Asiakas {asiakas.ToString()} maksaa ostoksensa";
+        }
+        public string PoistuJonosta()
+        {
+            return $"{Jono.Peek()} lähti jonosta. {Jono.Dequeue()}";
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Kassajono alepanJono = new Kassajono();
+            string nimi = "";
+            do
+            {
+                Console.WriteLine("Anna jonoon tulevan asiakkaan nimi (enter lopettaa):");
+                nimi = Console.ReadLine();
+                if (nimi != "")
+                {
+                    alepanJono.MeneJonoon(new Asiakkaat() { Name = nimi });
+                }
+                Console.WriteLine($"Alepan jonossa on nyt {alepanJono.Pituus} asiakasta");
+                foreach (Asiakkaat item in alepanJono.Jono)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            } while (nimi != "");
+            while (alepanJono.Pituus > 0)
+            {
+                Console.WriteLine("----- Palvellaan jonon ensimmäistä asiakasta -----");
+                Console.WriteLine("Palvelen asiakasta:" + alepanJono.Jono.Peek());
+                Console.WriteLine(alepanJono.MaksaOstokset(alepanJono.Jono.Peek()));
+                alepanJono.PoistuJonosta();
+                Console.WriteLine($"Alepan jonossa on nyt {alepanJono.Pituus} asiakasta");
+            }
+            Console.WriteLine("Jono on nyt tyhjä.");
         }
     }
 }
