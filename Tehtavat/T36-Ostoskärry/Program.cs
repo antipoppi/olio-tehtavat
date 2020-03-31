21c1bf60df5b95fb747d35871ec24dddd2f9bb35
N4927 <N4927@student.jamk.fi>
2020-03-31 15:57:34 +0300

    Added T36-Ostoskarry

---
+++ Tehtavat/T36-Ostoskärry/Program.cs
@@ -0,0 +1,67 @@
+﻿/* T36-Ostoskärry
+ * Tehtävänanto:
+ * 
+ * Toteuta sovellus, jolla voit hallita ostoskärryn sisältöä. 
+ * Ostettavalta tuotteelta riittää käsitellä nimi ja hinta.
+ * Toteuta Product-luokka ja lisää pääohjelmassa esimerkiksi List-tietorakenteeseen muutamia Product-luokan oliota. 
+ * Tulosta lopuksi kokoelman sisältö.
+ * 
+ * Testaa yksikkötestauksen avulla, että ostoskärry ilmoittaa oikean määrän tuotteita. 
+ * Tee ja suorita yksikkötestit 0,1,2 ja 5 tuotetta varten.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace T36_Ostoskärry
+{
+    public class ShoppingCart
+    {
+        public List<Product> Cart { get; set; }
+        public ShoppingCart()
+        {
+            Cart = new List<Product>();
+        }
+        public string PrintAll()
+        {
+            string r = null;
+            r += "Your products in shopping cart:";
+            foreach (Product item in Cart)
+            {
+                r += "\n- product : " + item.ToString();
+            }
+            r += $"\nThere are {Cart.Count} products in shopping cart.";
+            return r;
+        }
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"{Name} {Price}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create shopping cart
+            ShoppingCart ostoskori = new ShoppingCart();
+            // add items to it
+            ostoskori.Cart.Add(new Product("Maitopurkki", 1.15));
+            ostoskori.Cart.Add(new Product("Jauheliha", 3.45));
+            ostoskori.Cart.Add(new Product("Voi", 1.75));
+            ostoskori.Cart.Add(new Product("Banaani", 1.45));
+            ostoskori.Cart.Add(new Product("Kurkku", 0.87));
+            ostoskori.Cart.Add(new Product("Tomaatti", 1.34));
+            // print all products
+            Console.WriteLine(ostoskori.PrintAll());
+        }
+    }
+}
---
+++ Tehtavat/T36-OstoskärryTests/ShoppingCartTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using T36_Ostoskärry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T36_Ostoskärry.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void ShoppingCartTest_0item()
+        {
+            // assign
+            ShoppingCart cart = new ShoppingCart();
+            int expected = 0;
+            // act
+            int actual = cart.Cart.Count;
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ShoppingCartTest_1item()
+        {
+            // assign
+            ShoppingCart cart = new ShoppingCart();
+            cart.Cart.Add(new Product("item", 1.0));
+            int expected = 1;
+            // act
+            int actual = cart.Cart.Count;
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ShoppingCartTest_2item()
+        {
+            // assign
+            ShoppingCart cart = new ShoppingCart();
+            cart.Cart.Add(new Product("item", 1.0));
+            cart.Cart.Add(new Product("item", 1.0));
+            int expected = 2;
+            // act
+            int actual = cart.Cart.Count;
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ShoppingCartTest_5item()
+        {
+            // assign
+            ShoppingCart cart = new ShoppingCart();
+            cart.Cart.Add(new Product("item1", 1.0));
+            cart.Cart.Add(new Product("item2", 1.0));
+            cart.Cart.Add(new Product("item3", 1.0));
+            cart.Cart.Add(new Product("item4", 1.0));
+            cart.Cart.Add(new Product("item5", 1.0));
+            int expected = 5;
+            // act
+            int actual = cart.Cart.Count;
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
