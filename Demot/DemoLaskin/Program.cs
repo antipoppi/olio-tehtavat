0c40b71027c62dfd6116879b7c914987bddd3157
N4927 <N4927@student.jamk.fi>
2020-03-31 14:47:38 +0300

    added DemoLaskin (test)

---
+++ Demot/DemoLaskin/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DemoLaskin
+{
+    interface ICalculator
+    {
+        int Add(int number1, int number2);
+        int Multiply(int number1, int number2);
+    }
+    public class Calculator : ICalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Demot/DemoLaskinTests/CalculatorTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoLaskin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLaskin.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            // AAA-periaate
+            // Arrange
+            DemoLaskin.Calculator calc = new Calculator();
+            int a = 5;
+            int b = 6;
+            int expected = 11;
+
+            // Act
+            int result = calc.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            // Arrange
+            DemoLaskin.Calculator calc = new Calculator();
+            int a = 5;
+            int b = 6;
+            int expected = 30;
+
+            // Act
+            int result = calc.Multiply(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void AddTest0()
+        {
+            // AAA-periaate
+            // Arrange
+            DemoLaskin.Calculator calc = new Calculator();
+            int a = 5;
+            int b = 0;
+            int expected = 5;
+
+            // Act
+            int result = calc.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
