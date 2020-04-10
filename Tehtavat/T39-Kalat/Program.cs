5edd7c08e239d5738ce360defad470dc17b3be85
N4927 <N4927@student.jamk.fi>
2020-04-10 14:58:12 +0300

    T39-Kalat done

--- Tehtavat/T39-Kalat/Program.cs
+++ Tehtavat/T39-Kalat/Program.cs
@@ -21,8 +21,8 @@ namespace T39_Kalat
 {
     public class Fisherman
     {
-        public string Name { get; private set; }
-        public string Phone { get; private set; }
+        public string Name { get; }
+        public string Phone { get; }
         public Fisherman(string name, string phone)
         {
             Name = name;
@@ -35,9 +35,9 @@ namespace T39_Kalat
     }
     public class Fish
     {
-        public string Specie { get; private set; }
-        public double Length { get; private set; }
-        public double Weight { get; private set; }
+        public string Specie { get; }
+        public double Length { get; }
+        public double Weight { get; }
         public Fish(string specie, double length, double weight)
         {
             Specie = specie;
@@ -51,8 +51,8 @@ namespace T39_Kalat
     }
     public class Location
     {
-        public string Place { get; private set; }
-        public string LocationName { get; private set; }
+        public string Place { get; }
+        public string LocationName { get; }
         public Location(string place, string location)
         {
             Place = place;
@@ -65,9 +65,9 @@ namespace T39_Kalat
     }
     public class FishingEvent
     {
-        public Fisherman Fisher { get; private set; }
-        public Fish Fish { get; private set; }
-        public Location Location { get; private set; }
+        public Fisherman Fisher { get; }
+        public Fish Fish { get; }
+        public Location Location { get; }
         public FishingEvent(Fisherman fisherman, Fish fish, Location location)
         {
             Fisher = fisherman;
@@ -77,7 +77,7 @@ namespace T39_Kalat
     }
     public class FishRegister
     {
-        public List<Fisherman> Fishers { get; private set; }
+        public List<Fisherman> Fishers { get; }
         public List<FishingEvent> FishingEventList { get; set; }
         public FishRegister()
         {
@@ -90,20 +90,25 @@ namespace T39_Kalat
     {
         static void Main(string[] args)
         {
+            // create a new FishRegister and add some fishers to it
             FishRegister myFishRegister = new FishRegister();
             AddFisher(myFishRegister, new Fisherman("Tatu Alatalo", "0401234567"));
             AddFisher(myFishRegister, new Fisherman("Matti Meikäläinen", "0407654321"));
+            // add some fishes to FishRegister
             AddFish(myFishRegister, "Tatu Alatalo", new Fish("Pike", 0.62, 1.52), new Location("Pyhäjoki", "Kärsämäki"));
-            AddFish(myFishRegister, "Matti Meikäläinen", new Fish("Pike", 0.98, 3.15), new Location("Pyhäjoki", "Kärsämäki"));
-            // tähän tulostus jossa näkyy kalat per kalastaja esim PrintMyFishApp()
+            AddFish(myFishRegister, "Matti Meikäläinen", new Fish("Pike", 0.98, 3.15), new Location("Pyhäjärvi", "Pyhäjärvi"));
+            AddFish(myFishRegister, "Tatu Alatalo", new Fish("Salmon", 0.51, 1.12), new Location("Kalajoki", "Ylivieska"));
+            // print FishRegister
+            PrintMyFishApp(myFishRegister);
+            // sort fishes by weight and print the register
             PrintBiggestFishes(myFishRegister);
         }
-        public static void AddFisher(FishRegister myFishRegister, Fisherman fisher)
+        private static void AddFisher(FishRegister myFishRegister, Fisherman fisher)
         {
             myFishRegister.Fishers.Add(fisher);
             Console.WriteLine($"Fisher {fisher.Name} added into the Fishregister.\n");
         }
-        public static void AddFish(FishRegister myFishRegister, string fisher, Fish fish, Location location)
+        private static void AddFish(FishRegister myFishRegister, string fisher, Fish fish, Location location)
         {
             Fisherman fisherMan = myFishRegister.Fishers.Find(x => x.Name == fisher);
             if (fisherMan != null)
@@ -114,16 +119,32 @@ namespace T39_Kalat
             Console.WriteLine(fish.ToString());
             Console.WriteLine(location.ToString() + "\n");
         }
-
-
-        public static void PrintBiggestFishes(FishRegister myFishRegister)
+        private static void PrintMyFishApp(FishRegister myFishRegister)
+        {
+            Console.WriteLine("\n*** All the fishes sorted by fishermen: ***\n");
+            for (int i = 0; i < myFishRegister.Fishers.Count; i++)
+            {
+                string fisherName = myFishRegister.Fishers[i].Name;
+                var hold = myFishRegister.FishingEventList.FindAll(x => x.Fisher.Name == fisherName).ToArray();
+                if (hold != null)
+                {
+                    foreach (var item in hold)
+                    {
+                        Console.WriteLine(item.Fish.ToString());
+                        Console.WriteLine(item.Location.ToString());
+                        Console.WriteLine(item.Fisher.ToString() + "\n");
+                    }
+                }
+            }
+        }
+        private static void PrintBiggestFishes(FishRegister myFishRegister)
         {
             FishRegister holderRegister = new FishRegister();
             holderRegister = myFishRegister;
             var hold = holderRegister.FishingEventList.OrderByDescending(x => x.Fish.Weight).ToList(); // tää oli tehtävän vaikein juttu. Kätevää ja toimii
             if (hold != null)
                 holderRegister.FishingEventList = hold;
-            Console.WriteLine("\n*** All fish in Fishregister: ***\n");
+            Console.WriteLine("\n*** All fishes sorted by weight in the Fishregister: ***\n");
             foreach (var item in holderRegister.FishingEventList)
             {
                 Console.WriteLine(item.Fish.ToString());
